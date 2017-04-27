a7ac18d9f29df8509086e773bc4f5ff8683c30f1
sesteves <sroesteves@gmail.com>
2017-04-27 18:28:08 +0100

    added stats information

--- src/pt/inescid/gsd/ssb/Main.java
+++ src/pt/inescid/gsd/ssb/Main.java
@@ -1,5 +1,7 @@
 package pt.inescid.gsd.ssb;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,7 +29,12 @@ public class Main {
         COLUMN, ROW
     }
 
-    private static SequenceType sequenceType = SequenceType.ROW;
+    private static final SequenceType sequenceType = SequenceType.ROW;
+
+    private static final String statsFName = String.format("stats-%d.csv", System.currentTimeMillis());
+
+    private static final String STATS_HEADER = "runtime,seqtype,seqminsize,seqmaxsize,blocksize,zipfn,zipfe,nops,"
+        + "timestamp,op,latency";
 
     private static final String[] TABLES = { "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9" };
     private static final String[] FAMILIES = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
@@ -39,6 +46,7 @@ public class Main {
     private static final int MAX_SEQUENCES = 1;
     private static final int MIN_SEQUENCE_ITEMS = 3;
     private static final int MAX_SEQUENCE_ITEMS = 20;
+    private static final int BLOCK_SIZE = 1000;
     // private static final double FREQ_SEQUENCE_RATIO = 0.5;
 
     private static List<List<DataContainer>> sequences;
@@ -52,6 +60,11 @@ public class Main {
     private static Map<String, HTable> htables = new HashMap<>();
 
     private static boolean tablesCreated = false;
+
+    private static BufferedWriter statsF;
+
+    private static int zipfn = 100;
+    private static double zipfe = 3;
 
     private static void init() throws IOException {
         final Configuration config = HBaseConfiguration.create();
@@ -70,6 +83,15 @@ public class Main {
 
             // htable.setAutoFlush(false);
             // htable.setWriteBufferSize(1024 * 1024 * 12);
+
+
+            statsF = new BufferedWriter(new FileWriter(statsFName));
+            statsF.write(STATS_HEADER);
+            statsF.newLine();
+            statsF.write("," + sequenceType + "," + MIN_SEQUENCE_ITEMS + "," + MAX_SEQUENCE_ITEMS + "," + BLOCK_SIZE +
+                    "," + zipfn + "," + zipfe + "," + MAX_WAVES + ",,,");
+            statsF.newLine();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -101,7 +123,7 @@ public class Main {
         if (!tablesCreated)
             return;
 
-        byte[] block = new byte[1000];
+        byte[] block = new byte[BLOCK_SIZE];
         random.nextBytes(block);
 
         System.out.println("Populating...");
@@ -164,7 +186,7 @@ public class Main {
         Random random = new Random(100);
 
         // exponent is linked to number of frequent sequences
-        ZipfDistribution zipf = new ZipfDistribution(100, 3);
+        ZipfDistribution zipf = new ZipfDistribution(zipfn, zipfe);
 
         for (int wave = 0; wave < MAX_WAVES; wave++) {
 
@@ -177,7 +199,14 @@ public class Main {
                     // TODO: measure request latency and throughtput
                     Get get = new Get(dc.getRow());
                     get.addColumn(dc.getFamily(), dc.getQualifier());
-                    htables.get(Bytes.toString(dc.getTable())).get(get);
+
+                    long startTick = System.currentTimeMillis();
+                    htables.get(dc.getTableStr()).get(get);
+                    long endTick = System.currentTimeMillis();
+                    long diff = endTick - startTick;
+
+                    statsF.write(",,,,,,,," + endTick + ",g," + diff + "\n");
+
                 }
             } else {
                 int size = MIN_SEQUENCE_ITEMS + random.nextInt(MAX_SEQUENCE_ITEMS);
@@ -202,8 +231,9 @@ public class Main {
         long startTick = System.currentTimeMillis();
         runWorkload();
         long endTick = System.currentTimeMillis();
-
-        System.out.println("Time taken: " + (endTick - startTick));
-
+        long diff = endTick - startTick;
+        System.out.println("Time taken: " + diff);
+        statsF.write(diff + ",,,,,,,,,\n");
+        statsF.close();
     }
 }
