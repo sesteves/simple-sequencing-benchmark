d2c6f5e1b843cfe0807ecfc080af28f41f7fb6ab
sesteves <sroesteves@gmail.com>
2017-06-29 17:44:28 +0100

    minor

--- run.bash
+++ run.bash
@@ -17,21 +17,6 @@ fname=stats-all-$app-$(date +%s).csv
 header="enabled,seqssize,seqtype,seqminsize,seqmaxsize,blocksize,zipfn,zipfe,nops"
 printheader=true
 
-
-for enabled in 'false' 'true'; do
-  for zipfe in 0.5 1.0 1.5 2.0 2.5 3; do
-    if $enabled ; then
-      for heuristic in 'fetch-all' 'fetch-top-n' 'fetch-progressively'; do
-        echo "Executing... (enabled: true, zipfe: $zipfe, heuristic: $heuristic)"
-        execute
-      done
-    else
-      echo "Executing... (enabled: false, zipfe: $zipfe)"
-      execute
-    fi
-  done
-done
-
 function execute {
   java -cp lib/*:../cache-mining/lib/*:resources/:out/:. -Denabled=$enabled -Dcache-size=$cachesize -Dheuristic=$heuristic pt.inescid.gsd.ssb.Benchmark $seqssize $seqtype $seqminsize $seqmaxsize $blocksize $zipfn $zipfe $nops
 
@@ -45,3 +30,18 @@ function execute {
     printheader=false
   fi
 }
+
+for enabled in 'false' 'true'; do
+  for zipfe in 0.5 1.0 1.5 2.0 2.5 3; do
+    if $enabled ; then
+      for heuristic in 'fetch-all' 'fetch-top-n' 'fetch-progressively'; do
+        echo "Executing... (enabled: true, zipfe: $zipfe, heuristic: $heuristic)"
+        execute
+      done
+    else
+      echo "Executing... (enabled: false, zipfe: $zipfe)"
+      heuristic='none'
+      execute
+    fi
+  done
+done
