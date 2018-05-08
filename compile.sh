a516ef6e942512133b8f2dce6982e9e21739a06e
sesteves <sroesteves@gmail.com>
2018-05-08 02:23:36 +0100

    minor

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-javac -d out/ -cp lib/*:../cache-mining/lib/*:out/:. src/pt/inescid/gsd/ssb/*
+javac -d out/ -cp lib/*:../cache-mining/lib/*:out/:spmf/spmf.jar:. src/pt/inescid/gsd/ssb/*
 scalac -d out src/*.scala
